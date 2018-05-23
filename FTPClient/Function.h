048accd1b2af9e5cc775b211a87e096c6a7688c8
Nghĩa <tnghia14111998@gmail.com>
2018-05-23 12:57:29 +0700

    Add files via upload

--- FTPClient/Function.h
+++ FTPClient/Function.h
@@ -5,11 +5,10 @@
 #include <iostream>
 #include <sstream>
 #include <fstream>
+#include "dirent.h"
 #include "FTPClient.h"
 #define MAX_BUFFER 1024
 using namespace std;
-//Analyze the command and send to server
-void sendCommandToServer(CSocket&,string);
 //Receive a message from FTP server
 string receiveMessage(CSocket& sock);
 //Extract code from server message
@@ -17,9 +16,9 @@ int getMessageCode(string);
 //Login to server
 void logIn(CSocket& sock);
 //Get file from server
-void getFile(CSocket&, string);
+bool getFile(CSocket&,CSocket&, string,bool);
 //Upload a file to server
-void uploadFile(CSocket&, string);
+bool uploadFile(CSocket&,CSocket&, string,bool);
 //Create new folder
 void createFolder(CSocket&, string);
 //Delete a file in server
@@ -34,3 +33,29 @@ void changeServerPath(CSocket&, string);
 void changeClientPath(string);
 //Get parameter from a command
 string getParameter(stringstream&, string);
+//Connect to data port
+bool connectDataPort(CSocket& sock,CSocket& ClientData, bool passive);
+//Create new folder
+void createFolder(CSocket&, string);
+//Delete a file in server
+void deleteFile(CSocket&, string);
+//Delete empty folder in server
+void deleteEmptyFolder(CSocket&, string);
+//Listing file and folder in server
+bool getFileList(CSocket&, CSocket&,string, bool, vector<string>&);
+//Listing file and folder detail in server
+string getFileListDetail(CSocket&, CSocket&,string, bool);
+//Analyze the ls command and return list of files
+vector<string> analyzeFileList(string);
+//Delete multiple files
+void deleteMultipleFiles(CSocket&, CSocket&,CSocket&, stringstream&, bool);
+//Delete space at begin and end of the command
+void deleteSpaces(string&);
+//Download multiple files
+void getMultipleFiles(CSocket&, CSocket&,CSocket&, stringstream&, bool);
+//Upload multiple files
+void uploadMultipleFiles(CSocket&, CSocket&, CSocket&, stringstream&, bool);
+//Get file list in a folder in client
+bool getFileListClient(string,vector<string>&);
+//print file
+void printFileList(vector<string>);
